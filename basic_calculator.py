4651a9c511dac61861df24c831079075b97e2307
Felix Asante <116985938+fasante241@users.noreply.github.com>
2025-08-19 16:52:03 -0400

    Add files via upload

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,70 +1,36 @@
+import sys
+
+
 # Calculator
 
-#Menu Function
-print('''
-        Menu Options
-        1. Addition
-        2. Subtraction
-        3. Multiplication
-        4. Division
-    ''')
-options = input('Select from the options above: ')
- 
- # Take two numbers 
-first_number = float(input('Enter your first number: '))    
-second_number = float(input('Enter your second number: '))
+def calculator(first_number, second_number):
+
+# show the list of options
+    print('''Choose from the options below: 
+      1. Addition
+      2. Subtraction
+      3. Multiplication
+      4. Division''')
+
+    select = input("Select from the options above: ")
+
+    formula = {
+        "1" : first_number + second_number,
+        "2" : first_number - second_number,
+        "3" : first_number * second_number,
+        "4" : first_number / second_number,
+        }
+
+    results = formula.get(select, "Invalid Input")
+    print(round(results, 2))
 
 
+if __name__ == "__main__":
+    print(sys.argv[0])
+    first_number = float(input("Enter your first number:"))
+    second_number = float(input("Enter your second number:"))
+    calculator(first_number, second_number)
 
-class Calculate():
-    def __init__(self, first_number, second_number):
-        self.first_number = first_number
-        self.second_number = second_number
-        
-
-    def addition(self):
-        self.results = self.first_number + self.second_number 
-        print(f'The answer is {self.results}')
-    
-    def subtraction(self):
-        self.results = self.first_number - self.second_number 
-        print(f'The answer is {self.results}')
-
-    def multiplication(self):
-        self.results = self.first_number * self.second_number 
-        print(f'The answer is {self.results}')
-    
-    def division(self):
-        self.results = self.first_number / self.second_number 
-        print(f'The answer is {self.results}')
-    
-
-# Set Conditions
-def conditions(options):
-
-    if options == '1':
-        numbers = Calculate(first_number, second_number)
-        numbers.addition()
-
-    elif options == '2':
-        numbers = Calculate(first_number, second_number)
-        numbers.subtraction()
-
-    elif options == '3':
-        numbers = Calculate(first_number, second_number)
-        numbers.multiplication()
-
-    elif options == '4':
-        numbers = Calculate(first_number, second_number)
-        numbers.division()
-
-
-conditions(options)
-
-
-
-
-    
    
     
 
